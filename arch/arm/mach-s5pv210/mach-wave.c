d712d0d8df0526512d25ee52a7d546f0318865bb
Alberto96 <albertoguanti@gmail.com>
2014-02-26 20:03:09 +0100

    Revert "wave: revert Refresh rate hack for Wave 2 Display"

--- arch/arm/mach-s5pv210/mach-wave.c
+++ arch/arm/mach-s5pv210/mach-wave.c
@@ -257,7 +257,7 @@ static struct s3cfb_lcd lg4573 = {
 	.p_width 	= 52,	//  width of lcd in mm
 	.p_height 	= 86,	//  height of lcd in mm
 	.bpp 		= 24,   //
-	.freq 		= 60,	//
+	.freq 		= 68,	//
 	.timing = {
 
 		.h_fp 	= 17,
